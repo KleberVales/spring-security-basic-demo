eab51374fdc53c96787184e0bea2d74b8d55350d
Kleber Vales <klebervales.dev@gmail.com>
2025-08-15 10:48:41 -0300

    build: starter spring boot with java 21

---
+++ src/test/java/com/example/springsecuritybasicdemo/SpringSecurityBasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
